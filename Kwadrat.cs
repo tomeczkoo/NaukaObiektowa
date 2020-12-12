6f29c15bd7328c43b4543a4298332112373a5712
tomeczkoo <worek9876@wp.pl>
2020-12-13 00:31:31 +0100

    over

---
+++ Kwadrat.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Fajguji
+{
+
+	public class Kwadrat : Figura 
+	{
+		public Kwadrat(int a)
+		{
+			bokA = a;
+			
+		}
+		public int obliczPole()
+		{
+			return bokA * bokA;
+		}
+		public int obliczObwod()
+		{
+			return bokA * 4;
+		}
+		
+	}
+}
